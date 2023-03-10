7c0a3ed4cdf9a5f09aa0d4930cef3ee47d7e01e7
Fernando Parra <fernandoxe@gmail.com>
2023-03-10 22:52:43 +0100

    chore: socket io

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('asdasdas');
+import { Server } from 'socket.io';
+
+const io = new Server(5000, {
+  cors: {
+    origin: [
+      'http://localhost:3000'
+    ],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('join', (message) => {
+    socket.data.user = message.user;
+    socket.data.room = message.room;
+    socket.join(socket.data.room);
+    socket.to(socket.data.room).emit('user-connected', {user: socket.data.user});
+    console.log('clients >>>>');
+    io.sockets.sockets.forEach(socket => console.log('  >', socket.id, socket.data.user));
+  });
+
+  socket.on('play', (message) => {
+    console.log('user plays', socket.data.user);
+    console.log(socket.data.user, socket.data.room);
+    socket.to(socket.data.room).emit('play', {user: message.user});
+  });
+
+  socket.on('pause', (message) => {
+    console.log('user paused', socket.data.user);
+    console.log(socket.data.user, socket.data.room);
+    socket.to(socket.data.room).emit('pause', {user: message.user});
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log('socket disconnect', socket.id, reason);
+  });
+});
